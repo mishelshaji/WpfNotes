23430e29247f4b105b11b1ac26e69e3872450539
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2024-12-06 12:36:31 +0530

    Added Keyframes animation projects

---
+++ KeyframeAnimationOnCanvas/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyframeAnimationOnCanvas;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void MyRectangle_OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        var animation = (Storyboard)FindResource("Animation");
+        animation.Begin();
+    }
+}
