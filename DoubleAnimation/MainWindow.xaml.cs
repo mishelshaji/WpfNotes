22af84094b12edcdf195ed7d42f1eb794b643c06
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2024-12-06 11:01:38 +0530

    Added double animation projects

--- DoubleAnimation/MainWindow.xaml.cs
+++ DoubleAnimation/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1;
+namespace DoubleAnimation;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
