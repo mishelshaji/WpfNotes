ff3c950eb8d8ebb4b678676528b8d268889e1e5e
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2024-12-09 11:04:31 +0530

    Added event handling projects

---
+++ EventTunneling/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventTunneling;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void StackPanel_OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        MessageBox.Show("Event handled by stack panel");
+        // e.Handled = true;
+    }
+
+    private void Button_OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        MessageBox.Show("Event handled by button");
+    }
+}
