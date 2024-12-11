a1cc8126dcbf165977f95cd338c4a94bc4eba459
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2024-12-11 12:28:20 +0530

    Renamed projects

---
+++ DispatcherWithThreads/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DispatcherWithThreads;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        var t = new Thread(DoThis);
+        t.Start();
+    }
+
+    private void DoThis()
+    {
+        Thread.Sleep(5000);
+        Dispatcher.Invoke(() =>
+        {
+            TxtMessage.Text = "Text updated after 5 seconds.";
+        });
+    }
+}
