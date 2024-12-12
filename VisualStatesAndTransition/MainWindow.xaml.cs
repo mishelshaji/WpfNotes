f16e76b5249d8697619ab7dd916c1b8b79308d25
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2024-12-12 16:21:47 +0530

    Added Visual State Manager project

---
+++ VisualStatesAndTransition/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VisualStatesAndTransition;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void UIElement_OnMouseEnter(object sender, MouseEventArgs e)
+    {
+        VisualStateManager.GoToState(this, "MouseOver", true);
+    }
+
+    private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        VisualStateManager.GoToState(this, "Pressed", true);
+    }
+
+    private void UIElement_OnMouseUp(object sender, MouseButtonEventArgs e)
+    {
+        VisualStateManager.GoToState(this, "Normal", true);
+    }
+}
