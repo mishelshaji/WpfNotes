479974d6df33625230e8a4687643d4be396b809d
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2024-12-13 16:06:36 +0530

    Added CustomControl project

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,1 @@
-﻿var obj1 = new Person() { Name = "Person 1" };
-var obj2 = new Person() { Name = "Person 2" };
-var obj3 = new Person() { Name = "Person 3" };
-
-var propInfo = obj1.GetType().GetProperty("Name");
-var value = propInfo.GetValue(obj1);
-Console.WriteLine(value);
-
-class Person
-{
-    public string Name { get; set; }
-}
+﻿Console.WriteLine();
---
+++ SimpleCustomControl/Controls/DoubleBorderButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SimpleCustomControl.Controls;
+
+public class DoubleBorderButton: ContentControl
+{
+    public DoubleBorderButton()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof(DoubleBorderButton),
+            new FrameworkPropertyMetadata(typeof(DoubleBorderButton)));
+    }
+}
